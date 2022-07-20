3765b48d1a9e85ec6b32415fb7232e3a088f6b8a
Moritz <moritz.mueller2@tu-dresden.de>
2022-07-20 11:08:38 +0200

    downloads translation models for respective language

--- recognize_stream.py
+++ recognize_stream.py
@@ -5,12 +5,21 @@ import sys
 import json
 import subprocess
 import argparse
-from transformers import MarianMTModel, MarianTokenizer, pipeline
+
+def get_marian_names(lang) -> (str, str):
+    #https://huggingface.co/Helsinki-NLP/opus-mt-en-de
+    #https://huggingface.co/Helsinki-NLP/opus-mt-de-e
+    marian_model_name_en = "Helsinki-NLP/opus-mt-en-de"
+    marian_directory_en = 'marian-translate-en-de'
+    marian_model_name_de = "Helsinki-NLP/opus-mt-de-en"
+    marian_directory_de = 'marian-translate-de-en'
+    if lang == "en":
+        return (marian_model_name_en, marian_directory_en, "translation_en_to_de")
+    else:
+        return (marian_model_name_de, marian_directory_de, "translation_de_to_en")
 
 SetLogLevel(-1)
-
 verbose = False
-
 if not sys.platform == "linux":
     sys.exit("Please use a linux OS.")
 
@@ -34,8 +43,6 @@ if args.list_devices:
     subprocess.run(['pactl', 'list', 'short', 'sources'])
     sys.exit(0)
 
-lang = "en"
-
 sample_rate=16000
 # Initialise recognizer
 if verbose:
@@ -43,13 +50,13 @@ if verbose:
 vosk_model_name_en = "vosk-model-en-us-0.22"
 vosk_model_name_de = "vosk-model-de-0.21"
 try:
-    if lang == "en":
-        model = Model(model_name=vosk_model_name_en)
+    if args.in_language == "en":
+        rec_model = Model(model_name=vosk_model_name_en)
     else:
-        model = Model(model_name=vosk_model_name_de)
+        rec_model = Model(model_name=vosk_model_name_de)
 except Exception:
-    sys.exit("No model could be downloaded!")
-rec = KaldiRecognizer(model, sample_rate)
+    sys.exit("Failed to download any Vosk model!")
+rec = KaldiRecognizer(rec_model, sample_rate)
 
 #for arnndn https://github.com/GregorR/rnnoise-models/tree/master/beguiling-drafter-2018-08-30
 #command = ('ffmpeg', '-loglevel', 'quiet', '-f', 'alsa', '-i', args.device,
@@ -62,10 +69,16 @@ stdout = ('-', ) #last part of the command
 #Initialise translator
 if verbose:
     print("Initialising translator...")
-directory = 'translate-en-de'
-tokenizer = MarianTokenizer.from_pretrained(directory)
-model = MarianMTModel.from_pretrained(directory)
-translator = pipeline("translation_en_to_de", model=model, tokenizer=tokenizer)
+marian_model_name, marian_directory, task = get_marian_names(args.in_language)
+if not os.path.exists(marian_directory):
+    trans_model = MarianMTModel.from_pretrained(marian_model_name)
+    tokenizer = MarianTokenizer.from_pretrained(marian_model_name)
+    tokenizer.save_pretrained(marian_directory)
+    trans_model.save_pretrained(marian_directory)
+else:
+    trans_model = MarianMTModel.from_pretrained(marian_directory)
+    tokenizer = MarianTokenizer.from_pretrained(marian_directory)
+translator = pipeline(task=task, model=trans_model, tokenizer=tokenizer)
 
 try:
     if verbose:
