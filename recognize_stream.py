c04f1a0c5feae63ea2f8b09b57d0681cb1fda33f
Moritz <moritz.mueller2@tu-dresden.de>
2022-08-22 18:18:43 +0200

    test commit

--- recognize_stream.py
+++ recognize_stream.py
@@ -157,7 +157,7 @@ def main():
         if record_process.poll() not in (None, 0): # should just be: if not None ?
             raise Exception("ffmpeg recorder failed to start!")
 
-        print('#' * 80)
+        print('#' * 90)
         print('Press Ctrl+C to stop recording')
         print('#' * 80)
         while True:
