703dd65b2a0a71d92a7f0bd549f200968b8abe3a
Moritz <moritz.mueller2@tu-dresden.de>
2022-09-09 15:31:58 +0200

    updated

--- install.sh
+++ install.sh
@@ -4,13 +4,15 @@
 
 
 venv_name="venv_speech_pipeline"
-sudo apt update && sudo apt install python3.10-venv ffmpeg mpv espeak-ng curl
+sudo apt-get update && sudo apt-get install python3-venv ffmpeg mpv espeak-ng curl
 echo "Creating python venv $HOME/$venv_name"
 python3 -m venv $HOME/$venv_name
 source $HOME/$venv_name/bin/activate
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 python3 recognize_stream.py --help
 normal=$(tput sgr0)
 green=$(tput setaf 2)
 echo "Try: $green python3 recognize_stream.py --in-language de tagesschau.mp4 $normal"
+echo "You have to activate the virtual environment: $green source $HOME/$venv_name/bin/activate $normal"
